70a651ebd57f4eaaaaec912f3ce4aad041efc7c0
Joshua Rechkemmer <joshua.rechkemmer48@gmail.com>
2023-02-04 17:33:17 -0600

    KeyItems following player

--- PsuedoDotRoot/Assets/Scripts/KeyItem.cs
+++ PsuedoDotRoot/Assets/Scripts/KeyItem.cs
@@ -2,24 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class KeyItem : MonoBehaviour
 {
     public GameObject targetObject;
     public string targetTag = "Player";
-    public float followSpeed = 2f;
+    public float followSpeed = 5f;
+    public float distFromTarget = .5f;
 
     // Update is called once per frame
     void Update()
     {
         if ( targetObject != null )
         {
-            transform.position = Vector2.MoveTowards(transform.position, targetObject.transform.position, Time.deltaTime * followSpeed);
+            if ( Vector2.Distance(transform.position, targetObject.transform.position) > distFromTarget )
+                transform.position = Vector2.MoveTowards(transform.position, targetObject.transform.position, Time.deltaTime * followSpeed);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == targetTag)
+        {
             targetObject = collision.gameObject;
+            // Send event/notification that key item was picked up
+        }
     }
 }
