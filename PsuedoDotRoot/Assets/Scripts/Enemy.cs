646485fb9ea12a953ad026702c99abcd907bd606
Reckhammer <joshua.rechkemmer48@gmail.com>
2023-02-05 02:55:16 -0600

    Implemented Enemy Patrol

--- PsuedoDotRoot/Assets/Scripts/Enemy.cs
+++ PsuedoDotRoot/Assets/Scripts/Enemy.cs
@@ -2,22 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
-    public GameObject playerRef;        // Reference to player game object
-    public float aggroDistance = 5f;    // Enemy will move towards Player once within this distance
-
-
-    // Just patrol
-
-    void Start()
+    enum EnemyStatus
     {
-        
+        Waiting,
+        Moving
     }
 
-    // Update is called once per frame
-    void Update()
+    public Rigidbody2D myRigidbody2D;   // Reference to this object's Rigidbody component
+    public GameObject[] waypoints;      // Positions that this will move between
+    public float speed = 10f;           // How fast this will move between points
+    public float waypointDelay = 2f;    // Time to wait before moving to other waypoint
+
+    private EnemyStatus currentStatus = EnemyStatus.Waiting;
+    private int currentPos;
+    private float timeElapsed = 0f;
+
+    private void Awake()
     {
-        
+        if ( myRigidbody2D == null )
+            myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if ( currentStatus == EnemyStatus.Moving )
+        {
+            EnemyMovement();
+        }
+        else if ( currentStatus == EnemyStatus.Waiting )
+        {
+            WaitForDelay();
+        }
+    }
+
+    private void EnemyMovement()
+    {
+        if ( Vector2.Distance(transform.position, waypoints[currentPos].transform.position) <= 0f )
+        {
+            // Destination is Reached
+            currentPos++;
+            if (currentPos >= waypoints.Length)
+            {
+                currentPos = 0;
+            }
+
+            currentStatus = EnemyStatus.Waiting;
+
+            return;
+        }
+
+        myRigidbody2D.position = Vector2.MoveTowards(transform.position, waypoints[currentPos].transform.position, Time.deltaTime * speed);
+    }
+
+    private void WaitForDelay()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if ( timeElapsed >= waypointDelay )
+        {
+            timeElapsed = 0f;
+            currentStatus = EnemyStatus.Moving;
+            return;
+        }
     }
 }
--- PsuedoDotRoot/Assets/Scripts/PlatformMove.cs
+++ PsuedoDotRoot/Assets/Scripts/PlatformMove.cs
@@ -7,7 +7,7 @@ public class PlatformMove : MonoBehaviour
 {
     public GameObject[] wayPoints;  // Positions that the platform will move between
     public float speed = 1f;        // How fast the platforms will move to get to waypoints
-    private int current = 0;        // Current waypoint that this is moving towards
+    private int currentPos = 0;        // Current waypoint that this is moving towards
 
     private Rigidbody2D myRigidbody2D;
 
@@ -18,15 +18,15 @@ public class PlatformMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if ( Vector2.Distance(transform.position, wayPoints[current].transform.position) <= 0f )
+        if ( Vector2.Distance(transform.position, wayPoints[currentPos].transform.position) <= 0f )
         {
-            current++;
-            if ( current >= wayPoints.Length )
+            currentPos++;
+            if ( currentPos >= wayPoints.Length )
             {
-                current = 0;
+                currentPos = 0;
             }
         }
 
-        myRigidbody2D.position = Vector2.MoveTowards(transform.position, wayPoints[current].transform.position, Time.deltaTime * speed);
+        myRigidbody2D.position = Vector2.MoveTowards(transform.position, wayPoints[currentPos].transform.position, Time.deltaTime * speed);
     }
 }
